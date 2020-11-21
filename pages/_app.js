79058a21e92edf88adb9455185e56a5a89a8c0ee
skyparci <skyparci@gmail.com>
2020-11-21 16:06:11 +0900

    Integrate typeScript

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
-import '../styles/globals.css'
+import dynamic from 'next/dynamic';
+import '../styles/globals.css';
+
+const DynamicComponentWithNoSSR = dynamic(() =>
+  import('../components/Wrapper'),
+  { ssr: false }
+);
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <DynamicComponentWithNoSSR>
+    <Component {...pageProps} />
+  </DynamicComponentWithNoSSR>;
 }
 
 export default MyApp
---
+++ pages/basic.js
@@ -0,0 +1,37 @@
+import { 
+    createCanvas, 
+    createHiddenCanvas,
+    fillEllipse
+} from '../helpers/canvas';
+import useMouseState from '../hooks/useMouseState';
+
+function Basic() {
+    const { ctx } = createCanvas('basic');
+    const { canvas: hiddenCanvas, ctx: hiddenCtx } = createHiddenCanvas('hidden');
+    
+    function draw(v) {
+        const { mouseX, mouseY } = v;
+        console.log('ctx', ctx)
+        ctx.fillStyle = 'red';
+        fillEllipse({
+            ctx,
+            width: mouseX,
+            height: mouseY,
+            width: 20,
+            height: 20
+        });
+    }
+
+    useMouseState({
+        draw
+    });
+
+    
+    hiddenCanvas.style.left = -window.innerWidth + 'px';
+
+    return (
+        <div>ss</div>
+    )
+}
+
+export default Basic
