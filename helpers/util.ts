4b362b40127770a454b52883aa742c04b9d4c6c7
skyparci <skyparci@gmail.com>
2020-11-22 23:42:12 +0900

    Add serializeForm helper

--- helpers/util.ts
+++ helpers/util.ts
@@ -1,2 +1,6 @@
 export const identity = <T>(v: T) => v;
 export const isNil = <T>(v: T) => v === undefined || v === null;
+export const serializeForm = (formElement: HTMLFormElement) => {
+    return Object.fromEntries(new FormData(formElement));
+
+}
--- pages/canvas/draw/Form.tsx
+++ pages/canvas/draw/Form.tsx
@@ -3,12 +3,19 @@ import FieldSet from '../../../components/Form/FieldSet'
 import TextField from '../../../components/Form/TextField'
 import ColorPicker from '../../../components/ColorPicker'
 import Button from '../../../components/Button'
+import { serializeForm } from '../../../helpers/util'
 
-function Form() {
+function Form({ onSubmit, size }) {
+    function handleSubmit(e) {
+        e.preventDefault();
+
+        onSubmit && onSubmit(serializeForm(e.currentTarget))
+    }
+
     return (
-        <form>
+        <form onSubmit={handleSubmit}>
             <FieldSet title="Size">
-                <TextField name="size" />
+                <TextField name="size" defaultValue={size} />
             </FieldSet>
             <FieldSet title="Color">
                 <ColorPicker />
