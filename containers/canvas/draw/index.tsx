35896a9f7f42b144beac3e02c1676d59b65ca3bc
skyparci <skyparci@gmail.com>
2020-11-28 18:14:21 +0900

    Move draw filtes to containers

---
+++ containers/canvas/draw/index.tsx
@@ -0,0 +1,45 @@
+import React, { useState } from 'react'
+import { Editor } from '../../../components/Layout'
+import { Shape } from '../../../components/ShapePicker'
+import Canvas from './Canvas'
+import Form from './Form'
+
+function Draw() {
+    const [{ size, color, shape, updateKey }, setState] = useState({
+        size: 20,
+        color: 'rgba(0, 0, 0, .5)',
+        shape: Shape.circle,
+        updateKey: 1
+    })
+
+    const handleSubmit = ({ size, color, shape }) => {
+        setState(prev => ({
+            ...prev,
+            size: size || prev.size,
+            color: color || prev.color,
+            shape: Number(shape)
+        }))
+    }
+
+    const handleReset = () => {
+        setState(prev => ({
+            ...prev,
+            updateKey: prev.updateKey + 1
+        }))
+    }
+
+    return (
+        <>
+            <Canvas size={size} color={color} shape={shape} key={updateKey} />
+            <Editor>
+                <Form 
+                    onSubmit={handleSubmit}
+                    onReset={handleReset}
+                    size={size}
+                />
+            </Editor>
+        </>
+    )
+}
+
+export default Draw
